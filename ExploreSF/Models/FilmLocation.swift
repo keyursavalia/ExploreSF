32f14a12e4440380a8a3c2fd322b7d868e34fdc1
Keyur <savalia.keyur1091@gmail.com>
2026-05-06 15:07:26 -0700

    update: remove init(from: MovieLocation) from FilmLocation

--- ExploreSF/Models/FilmLocation.swift
+++ ExploreSF/Models/FilmLocation.swift
@@ -9,13 +9,3 @@ struct FilmLocation: Identifiable, Hashable {
     let longitude: Double
 }
 
-extension FilmLocation {
-    init(from model: MovieLocation) {
-        self.id = model.id
-        self.title = model.title
-        self.releaseYear = model.releaseYear
-        self.locationName = model.locationName
-        self.latitude = model.latitude
-        self.longitude = model.longitude
-    }
-}
