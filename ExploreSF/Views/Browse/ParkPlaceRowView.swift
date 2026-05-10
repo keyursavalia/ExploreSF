b04795989d7d2a5495b24bdbad00e6197889ce24
Keyur <savalia.keyur1091@gmail.com>
2026-05-09 21:11:27 -0700

    update: ParkPlaceRowView thumbnail uses satellite MapSnapshotView

--- ExploreSF/Views/Browse/ParkPlaceRowView.swift
+++ ExploreSF/Views/Browse/ParkPlaceRowView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import MapKit
 
 struct ParkPlaceRowView: View {
     let place: ParkPlace
@@ -30,14 +31,16 @@ struct ParkPlaceRowView: View {
     }
 
     private var thumbnail: some View {
-        RoundedRectangle(cornerRadius: 8)
-            .fill(AppCategory.park.color.opacity(0.2))
-            .overlay(
-                Image(systemName: "tree")
-                    .font(.system(size: 20))
-                    .foregroundStyle(AppCategory.park.color)
-            )
-            .frame(width: 64, height: 80)
+        MapSnapshotView(
+            id: place.id,
+            coordinate: place.coordinate,
+            style: .parkSatellite,
+            category: .park,
+            cornerRadius: 8,
+            snapshotSize: CGSize(width: 64, height: 80),
+            iconSize: 20
+        )
+        .frame(width: 64, height: 80)
     }
 
     private var bodyText: some View {
